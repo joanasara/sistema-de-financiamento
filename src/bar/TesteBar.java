f6a3b130ddf1e900d1ec0a62db378e8c9ad5642d
joana sara <joanasousa678@gmail.com>
2020-07-07 23:13:35 -0300

    finacimento

---
+++ src/bar/TesteBar.java
@@ -0,0 +1,63 @@
+package bar;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class TesteBar {
+
+	public static void main(String[] args) {
+
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+
+		char sexo;
+		int cervejas, refri, espetos;
+		double consumo, couvert, ingresso, total;
+
+		// 1 LEITURA DOS DADOS
+
+		System.out.print("Sexo: ");
+		sexo = sc.next().charAt(0);
+		System.out.print("Quantidade de cervejas: ");
+		cervejas = sc.nextInt();
+		System.out.print("Quantidade de refrigerantes: ");
+		refri = sc.nextInt();
+		System.out.print("Quantidade de espetinhos: ");
+		espetos = sc.nextInt();
+
+		// 2 CALCULOS
+
+		if (sexo == 'F') {
+			ingresso = 8.0;
+		} else {
+			ingresso = 10.0;
+		}
+
+		consumo = cervejas * 5.0 + refri * 3.0 + espetos * 7.0;
+
+		if (consumo > 30) {
+			couvert = 0.0;
+		} else {
+			couvert = 4.0;
+		}
+
+		total = ingresso + consumo + couvert;
+
+		// 3 RELATORIO
+
+		System.out.println();
+		System.out.println("RELATÓRIO:");
+		System.out.printf("Consumo = R$ %.2f%n", consumo);
+		if (couvert == 0.0) {
+			System.out.println("Isento de Couvert");
+		} else {
+			System.out.printf("Couvert = R$ %.2f%n", couvert);
+		}
+		System.out.printf("Ingresso = R$ %.2f%n", ingresso);
+		System.out.println();
+		System.out.printf("Valor a pagar = R$ %.2f%n", total);
+
+		sc.close();
+
+	}
+}
